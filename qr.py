4804e4694fc61347f10da50a2b63155c2a9b162c
yamate-casino <114626397+yamate-casino@users.noreply.github.com>
2022-12-16 09:08:21 +0900

    Update qr.py

--- qr.py
+++ qr.py
@@ -3,5 +3,5 @@ import cv2
 img = qrcode.make("https://yamate-casino.github.io/artclass/qr.html")
 img.save("qr.png")
 img2 = cv2.imread("qr.png")
-cv2.imshow("Image",img2)
+cv2.imshow("Image",img)
 cv2.waitKey()
